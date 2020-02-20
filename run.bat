6a5a5655d83c5826b454e58a5c6e9a20a2282092
akuhn <adrian.kuhn@gmx.ch>
2020-02-20 10:28:49 +0100

    :bug: Changed path to conda environment in run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-C:\Users\Adrian\AppData\Local\ESRI\conda\envs\arcgispro-py3-clone\python.exe D:\UNIGIS\Masterarbeit\Code\tree-detection\treedetection.py D:\UNIGIS\Masterarbeit\Code\tree-detection\settings
+C:\Users\Adrian\AppData\Local\ESRI\conda\envs\arcgispro-py3-trees\python.exe D:\UNIGIS\Masterarbeit\Code\tree-detection\treedetection.py D:\UNIGIS\Masterarbeit\Code\tree-detection\settings
